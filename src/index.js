5f4dda6a09cb3b2f1c93b912726b7c5f257b8154
Avonavi23 <anna230997@mail.ru>
2021-08-22 18:44:48 +0400

    feat: add function expression-calculator

--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@ function eval() {
     // Do not use eval!!!
     return;
 }
-
-function expressionCalculator(expr) {
-    // write your solution here
+    
+function getFindedChars(arrStr, char) {
+    return arrStr.reduce((acc, item) => item === char ? acc += item : acc, '');
 }
-
+    
+function expressionCalculator(str) {
+    const arrStr = str.split('');
+    arrStr.forEach((item, i, arr) => {
+        if (item === '/' && arr[i + 2] === '0') {
+            throw new Error('TypeError: Division by zero.');
+        }
+    });
+    
+    const leftBr = getFindedChars(arrStr, '(');
+    const rightBr = getFindedChars(arrStr, ')');
+    
+    if (leftBr.length !== rightBr.length) {
+        throw new Error('ExpressionError: Brackets must be paired.');
+    }
+    
+    const result = (new Function(`return ${str}`))();
+    
+    return result;
+}
+    
 module.exports = {
     expressionCalculator
 }
+    
